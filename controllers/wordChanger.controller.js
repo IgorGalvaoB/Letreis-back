66b97371338d170758bc3f550c0fc312e4a6a9b6
Igor Galvao <igorgalvaob@gmail.com>
2022-08-03 22:16:22 -0300

    word changer

--- controllers/wordChanger.controller.js
+++ controllers/wordChanger.controller.js
@@ -1,19 +1,33 @@
 //const client = require('../config/redis.config')
+const Word = require('../model/Word.model')
+const client = require('../config/redis.config')
 
-const wordChanger = ()=>{
-    let date = null
-     word = 'aa'
-    const timeChecker = setInterval(()=>{
-    
-        if(!date) date = new Date('august 03,2022 23:10 GMT-3:00') 
-        const date2 = Date.now()
-        const dif = Math.floor((date2 - date)/1000/60)
-        
-        if (dif) {
-            this.clearInterval()
+
+let loop;
+
+const time = function(){
+    let initialTime = new Date()
+    const compare = async function (){
+        const currentTime = new Date()
+        const dif = currentTime.getMinutes() - initialTime.getMinutes()
+        if(dif){
+            clearInterval(loop)
+            initialTime = new Date()
+            await randomWord(compare)
             
         }
-        console.log(word)
-    },1000)
+        console.log(currentTime.getMinutes())
+    }
+    loop = setInterval(compare,1000)
+
+}()
+
+const randomWord = async (callback) =>{
+
+    const countWord = await Word.randomWord()
+    console.log(countWord)
+    loop = setInterval(callback,1000)
+
 }
-wordChanger()
+
+module.exports = time
--- model/Word.model.js
+++ model/Word.model.js
@@ -1,17 +1,31 @@
-const { Schema , model } = require('mongoose');
+const { Schema, model } = require('mongoose');
 
 const wordSchema = new Schema({
 
-    word:{
-    
-        type:String,
-        maxLength:6,
-        minLength:6,
-        lowercase:true,
-        trim:true,
-    
+    word: {
+
+        type: String,
+        maxLength: 6,
+        minLength: 6,
+        lowercase: true,
+        trim: true,
+
     }
+},
+    {
+        statics: {
+            async randomWord() {
+                const count = await this.count()
+                const rand = Math.floor(Math.random() * count);
+                const word = await this.findOne().skip(rand)
+                console.log(word,rand,count)
+                return word
+            }
+        }
+    }
+)
 
-})
 
-module.exports = model('Word',wordSchema)
+/*wordSchema.statics.randomWord = async ()=>{*/
+
+module.exports = model('Word', wordSchema)
