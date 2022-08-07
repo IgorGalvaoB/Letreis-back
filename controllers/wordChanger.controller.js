78b593ad0bd99eef8a49cf7a809dce9e590c3cae
Igor Galvao <igorgalvaob@gmail.com>
2022-08-07 12:11:37 -0300

    ###

--- controllers/wordChanger.controller.js
+++ controllers/wordChanger.controller.js
@@ -9,23 +9,24 @@ const time = function(){
     let initialTime = new Date()
     const compare = async function (){
         const currentTime = new Date()
-        const dif = currentTime.getDate() - initialTime.getDate()
+        const dif = currentTime.getMinutes() - initialTime.getMinutes()
         if(dif){
             clearInterval(loop)
             initialTime = new Date()
             await randomWord(compare)
             
+            
         }
         
     }
     loop = setInterval(compare,1000)
-
 }()
 
 const randomWord = async (callback) =>{
 
     const countWord = await Word.randomWord()
     await client.set('dayWord',countWord.word)
+    console.log(countWord)
     loop = setInterval(callback,1000)
 
 }
